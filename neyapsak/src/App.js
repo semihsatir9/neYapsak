e865468655c4bf7c957f596edf32899fe8fbdeae
Semih Satır <semihsatir9@hotmail.com>
2023-04-08 16:18:15 +0300

    Header inserted
    Header of the website inserted

--- neyapsak/src/App.js
+++ neyapsak/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+
 
 
 function App() {
-  return 
-  <div>
-    
-  </div>
+  return (
+
+    <Header/>
+  );
+  
 }
 
 export default App;
