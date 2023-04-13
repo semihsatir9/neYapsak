2c658adc8ac4807921eb84cb8983e0d35811a8d5
Semih Satır <semihsatir9@hotmail.com>
2023-04-13 16:41:37 +0300

    Unnecessary Icon removed

--- neyapsak/src/components/Header.js
+++ neyapsak/src/components/Header.js
@@ -30,22 +30,6 @@ export default function Header() {
     
       <AppBar position="static">
         <Toolbar>
-          {/*Inside the IconButton, we 
-           can render various icons*/}
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            {/*This is a simple Menu 
-             Icon wrapped in Icon */}
-            <MenuIcon />
-          </IconButton>
-          {/* The Typography component applies 
-           default font weights and sizes */}
-  
           <Typography variant="h6" 
             component="div" sx={{ flexGrow: 1 }}>
             neYapsak
