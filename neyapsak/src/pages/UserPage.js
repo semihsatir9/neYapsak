bda052c97240ee0a3684bedb1488f8971401361c
Gorkem-Boyaci <19COMP1027@isik.edu.tr>
2023-04-17 18:24:24 +0300

    Optimized the input type on the 3rd Q

--- neyapsak/src/pages/UserPage.js
+++ neyapsak/src/pages/UserPage.js
@@ -64,7 +64,15 @@ function UserPage() {
                 <label>Q2: How much time do you have to prepare dinner? (In Minutes)</label><br></br><br></br>
                 <input type = "int" id = "time" name="timeq"></input><br></br><br></br>
                 <label>Q3: What ingredients do you hate?</label><br></br><br></br>
-                <input type = "string" id = "hate" name="hateq"></input><br></br><br></br>
+                <input type = "checkbox" id = "hate1" name="hate1" value = "Rice"></input>
+                <label for = "hate1">Rice</label><br></br>
+                <input type = "checkbox" id = "hate2" name="hate2" value = "Rice"></input>
+                <label for = "hate2">Rice</label><br></br>
+                <input type = "checkbox" id = "hate3" name="hate3" value = "Rice"></input>
+                <label for = "hate3">Rice</label><br></br>
+                <input type = "checkbox" id = "hate4" name="hate4" value = "Rice"></input>
+                <label for = "hate4">Rice</label><br></br>
+                <br></br><br></br>
                 <label>Q4: How much calories can you tolarate? (Calorie calculation is based on the ingredients alone. Calories lost or gained on the cooking process of the meal is not accounted.)</label><br></br><br></br>
                 <input type = "int" id = "cal" name="calq"></input><br></br><br></br>
                 <button className="button align-right"><span>Answer</span></button>
